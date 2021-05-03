a4fe50a2c362c593bb5a21a537719bbd4dda30d8
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-05-03 14:24:44 +0300

    begining

---
+++ js/00-main.js
@@ -0,0 +1,9 @@
+/*
+MOKYMOSI TEMOS
+
+1.Kintamieji atsiminti darbinę informaciją
+2.palyginimo sąlygos
+3.ciklai - ėjimas per sąrašo narius
+4.funkcijos- pasikartojančios logikos panaudojimas
+5.Įvykiai- mouse click, keybord click, scroll, upload, etc.
+*/
