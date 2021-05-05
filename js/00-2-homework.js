15fe08115b1f92090354084e2d11d373b0a52faa
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-05-05 13:04:40 +0300

    classwork

---
+++ js/00-2-homework.js
@@ -0,0 +1,24 @@
+//         Ciklo for panaudojimas
+
+//Suskaičiuoti ką gausime susumavus skaičius intervale tarp (imtinai):
+let a=0;
+const b=4;
+
+for (let i = a-1 ; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+//0 - 0
+
+//0 - 4
+
+//0 - 100
+
+//574 - 815
+
+//-50 - 50
+
+//-70 - 30
+//panaudojant ciklą perrašyti tekstinio tipo kintamųjų reikšmes iš kito galo:
+//pvz.: “abcdef” -> “fedcba”
