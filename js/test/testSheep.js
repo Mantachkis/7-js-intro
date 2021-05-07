5da41dbcf65b124f14e21fc33bd1349a7126fcf7
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-05-07 20:38:46 +0300

    test

---
+++ js/test/testSheep.js
@@ -0,0 +1,19 @@
+function countSheeps(arrayOfSheep) {
+    var avis = 0;
+    for( let i = 0; i < arrayOfSheep.lenght; i++)
+      { 
+        if(arrayOfSheep[i]=== true);{
+          avis++;
+        }
+      
+     
+      }
+      return avis;
+  }
+
+  var array1 = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ];
