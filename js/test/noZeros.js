2bc5b03fa12545e5d80e3ce71980d204500db7a4
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-05-11 13:51:11 +0300

    nozero

--- js/test/noZeros.js
+++ js/test/noZeros.js
@@ -3,13 +3,19 @@ function noBoringZeros(n) {
     let newN= strN.split("");
     console.log(strN);
 
+    if(newN[0]===0){
+        return newN.join('')
+    }
+
     for(var i=newN.length - 1 ; i>0;i--)
     {
-
+        
         if(newN[i]==0)
         {
            newN.splice(i,1);
-         
+        }
+        else{
+            return newN.join('')
         }
     }
     
