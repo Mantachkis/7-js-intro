560eb5afe45263c603bec267fc1ccb93b41510c1
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-05-13 10:03:49 +0300

    test

---
+++ js/test/lochness.js
@@ -0,0 +1,20 @@
+
+function isLockNessMonster(s) {
+
+  
+  var vord = s.search("tree fiddy");
+  var num= s.search("3.50");
+if( vord>0){
+  return true}
+  else if (num>0){
+  return true}
+  else { return false}
+
+  
+
+  
+  
+}
+
+
+console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"));
---
+++ js/test/orderedCount.js
@@ -0,0 +1,15 @@
+var orderedCount = function (text) {
+   
+    let string=text.split('');
+
+    var sum=[];
+    for(var i=0;i<string.length;i++){
+        console.log(i);
+      if (string[i]== 'a'){
+        sum.push(['a', 5])
+      }
+    }
+console.log(sum);
+  }
+
+  console.log("abracadabra");
